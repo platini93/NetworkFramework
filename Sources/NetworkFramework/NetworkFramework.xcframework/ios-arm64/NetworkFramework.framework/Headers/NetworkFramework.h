7b360f76dfee969e91598c07f8c8f99eb878fb69
platini93 <platinimaldini789@gmail.com>
2024-04-07 16:09:44 +0530

    Initial Commit

---
+++ Sources/NetworkFramework/NetworkFramework.xcframework/ios-arm64/NetworkFramework.framework/Headers/NetworkFramework.h
@@ -0,0 +1,18 @@
+//
+//  NetworkFramework.h
+//  NetworkFramework
+//
+//  Created by Gourav Ray on 01/04/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkFramework.
+FOUNDATION_EXPORT double NetworkFrameworkVersionNumber;
+
+//! Project version string for NetworkFramework.
+FOUNDATION_EXPORT const unsigned char NetworkFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkFramework/PublicHeader.h>
+
+
